596788d2d82c636ce9014cfebce7925475c931b3
WuhanLLLiu <450994973@qq.com>
2020-03-01 09:55:14 +0800

    elementUI

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@
 import Vue from 'vue';
 //组件载入
 import App from './App.vue';
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
+Vue.use(ElementUI)
 const app = new Vue({
     render:h=>h(App)
 }).$mount("#app");
