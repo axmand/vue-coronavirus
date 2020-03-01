c4e8b9cf2e13825655825fd890874fcfccea2680
axmand <815966508@qq.com>
2020-03-01 09:32:57 +0800

    revise

--- src/main.js
+++ src/main.js
@@ -2,13 +2,8 @@
 import Vue from 'vue';
 //组件载入
 import App from './App.vue';
-//跨域请求
-import axios from 'axios';
 
 Vue.prototype.mapInstance = {};
-//跨域
-axios.defaults.headers.post["Content-type"]="application/json";
-Vue.prototype.$axios=axios;
 
 const app = new Vue({
     render:h=>h(App)
